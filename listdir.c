47036a4e8f558248b2b2a34e7b6b79ae2a2fcef0
Dmitriy Tochansky <tochansky@tochlab.net>
2016-12-23 10:31:08 +0300

    listdir: two ways of file read

--- listdir.c
+++ listdir.c
@@ -1,9 +1,47 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "lib/listdir.h"
 
-void test_file( char *filename ) {
-    printf("%s\n", filename);
+static int testedfiles = 0;
+static int sumsum = 0;
+
+void test_file_open(char *filename) {
+    char buf[2048];
+    int sum = 0;
+    //struct stat filestat;
+    int f = open(filename, O_RDONLY);
+    //fstat(fileno(f), &filestat);
+
+    lseek(f, testedfiles, SEEK_SET);
+    size_t readed = read(f, buf, 2048);
+    for(size_t i = 0;i<readed;i++) {
+        sum+=buf[i];
+    }
+    close(f);
+    testedfiles++;
+    sumsum += sum;
+    return;
+}
+
+void test_file_file(char *filename) {
+    char buf[2048];
+    int sum = 0;
+    //struct stat filestat;
+    FILE *f = fopen(filename, "r");
+    //fstat(fileno(f), &filestat);
+
+    fseek(f, testedfiles, SEEK_SET);
+    size_t readed = fread(buf, 1, 2048, f);
+    for(size_t i = 0;i<readed;i++) {
+        sum+=buf[i];
+    }
+    fclose(f);
+    testedfiles++;
+    sumsum += sum;
     return;
 }
 
@@ -17,5 +55,8 @@ int main(int argc, char**argv) {
         dirname = argv[1];
     }
     
-    list_dir(dirname, test_file);
+    list_dir(dirname, test_file_open);
+
+    printf("Tested files %d\n", testedfiles);
+    printf("SumSum %d\n", sumsum);
 }
